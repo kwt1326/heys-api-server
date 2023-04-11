593364d5e2af0360904d8ef4b1de4f11c4505fee
kwt1326 <u1326@hotmail.com>
2023-04-11 09:11:52 +0900

    fix: https swagger

--- src/main/kotlin/com/api/heys/HeysApplication.kt
+++ src/main/kotlin/com/api/heys/HeysApplication.kt
@@ -1,9 +1,11 @@
 package com.api.heys
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 
-
+@OpenAPIDefinition(servers = [Server(url = "/")])
 @SpringBootApplication
 class HeysApplication
 
